c08368bd1877d54e125a73302d2fc34c0a8c715a
Isaac Feldman <isaaccfeldman@gmail.com>
2022-06-21 19:45:19 -0400

    Modified eww and org-mode configs

--- init.el
+++ init.el
@@ -60,3 +60,20 @@
 (load-user-file "lisp/core.el")
 (load-user-file "lisp/documents.el")
 
+
+ (setq eww-retrieve-command
+           '("wget" "-Sq" "-O" "-"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- lisp/documents.el
+++ lisp/documents.el
@@ -2,32 +2,40 @@
 
 ;; --- ORG MODE ---
 ;; org-mode is fantastic as well. I keep things close to default.
-(setq org-directory "~/Dropbox/")
+(use-package org
+  :bind (("C-c l" . org-stored-link)
+         ("C-c a" . org-agenda)
+         ("C-c c" . org-capture))
+  :config
+  (setq org-directory "~/Dropbox/")
 
-(setq if/default-org-file "~/Dropbox/todo.org")
-;;(setq initial-buffer-choice "~/Dropbox/todo.org")
+  (setq if/default-org-file "~/Dropbox/todo.org")
 
-(setq org-agenda-files (directory-files-recursively "~/Dropbox/" "\.org$"))
-(setq org-todo-keywords
-      '((sequence  "TODO(t)" "PROJ(p)" "MAYBE(m)" "NEXT(n)" "|" "DONE(d)" "CANCELLED(c)" "DEFERRED(f)")))
+  (setq org-agenda-files
+        (directory-files-recursively "~/Dropbox/" "\.org$"))
+  (setq org-todo-keywords
+        '((sequence  "TODO(t)" "PROJ(p)" "MAYBE(m)" "NEXT(n)" "|"
+                     "DONE(d)" "CANCELLED(c)" "DEFERRED(f)")))
 
-;; Skip completed items
-(setq org-agenda-skip-deadline-if-done t)
-(setq org-agenda-skip-scheduled-if-done t)
-(setq org-agenda-skip-timestamp-if-done t)
-;; Skip deleted files
-(setq org-agenda-skip-unavailable-files t)
-(setq org-capture-templates
-      '(("t" "todo" entry (file+headline if/default-org-file "INBOX")
-         "* NEXT %?\n" :clock-in t :clock-resume t)
-        ("c" "caffeine" table-line (file+headline if/default-org-file "Caffeine")
-         "|%T|%?|")
-        ("m" "mood" table-line (file+headline "~/Dropbox/todo.org" "Mood")
-         "|%T|%^{Rating? (1: worst, 5: best|1|2|3|4|5}|%?|")))
-(setq org-image-actual-width nil)
+  (setq org-agenda-skip-deadline-if-done t)
+  (setq org-agenda-skip-scheduled-if-done t)
+  (setq org-agenda-skip-timestamp-if-done t)
+  ;; Skip deleted files
+  (setq org-agenda-skip-unavailable-files t)
+  (setq org-capture-templates
+        '(("t" "todo" entry (file+headline if/default-org-file "INBOX")
+           "* NEXT %?\n" :clock-in t :clock-resume t)
+          ("c" "caffeine" table-line
+           (file+headline if/default-org-file "Caffeine")
+           "|%T|%?|")
+          ("m" "mood" table-line
+           (file+headline "~/Dropbox/todo.org" "Mood")
+           "|%T|%^{Rating? (1: worst, 5: best|1|2|3|4|5}|%?|")))
+  
+  (setq org-image-actual-width nil)
+  (setq org-startup-indented t
+        org-src-tab-acts-natively t))
 
-(setq org-startup-indented t
-      org-src-tab-acts-natively t)
 (use-package org-pomodoro)
 
 ;; org-babel
@@ -37,7 +45,6 @@
     (python . t)
     (scheme . t)
     (gnuplot . t)))
-
 
 ;; read pdfs in emacs but better
 
